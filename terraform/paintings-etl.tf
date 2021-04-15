7f98c9a6335eaa0ecd81f0003f5380cfd85543ff
Cory Huebner <chuebner86@gmail.com>
2021-04-15 13:13:11 -0500

    add a workflow to crawl source, transform it, and crawl output

--- terraform/paintings-etl.tf
+++ terraform/paintings-etl.tf
@@ -12,35 +12,48 @@ data "aws_iam_policy_document" "glue_assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "csv_paintings_crawler" {
+data "aws_iam_policy_document" "paintings_etl" {
   statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.paintings_source.arn}/*"]
+    actions = ["s3:HeadObject", "s3:ListObjects", "s3:GetObject"]
+    resources = [
+      aws_s3_bucket.glue_jobs_source.arn,
+      "${aws_s3_bucket.glue_jobs_source.arn}/*",
+      aws_s3_bucket.paintings_source.arn,
+    "${aws_s3_bucket.paintings_source.arn}/*"]
+  }
+  statement {
+    actions = ["s3:ListBucket", "s3:HeadObject", "s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = [
+      aws_s3_bucket.paintings_processed.arn,
+      "${aws_s3_bucket.paintings_processed.arn}/*",
+      aws_s3_bucket.glue_temp_files.arn,
+      "${aws_s3_bucket.glue_temp_files.arn}/*",
+    ]
   }
 }
 
-resource "aws_iam_role" "csv_paintings_crawler" {
-  name                  = "${var.namespace}-csv-paintings-crawler"
+resource "aws_iam_role" "paintings_etl" {
+  name                  = "${var.namespace}-paintings-etl"
   assume_role_policy    = data.aws_iam_policy_document.glue_assume_role.json
   force_detach_policies = true
-  tags                  = merge(local.tags, { Name = "${var.namespace}-csv-paintings-crawler" })
+  tags                  = merge(local.tags, { Name = "${var.namespace}-paintings-etl" })
 }
 
-resource "aws_iam_role_policy" "csv_paintings_crawler" {
-  name   = aws_iam_role.csv_paintings_crawler.name
-  role   = aws_iam_role.csv_paintings_crawler.id
-  policy = data.aws_iam_policy_document.csv_paintings_crawler.json
+resource "aws_iam_role_policy" "paintings_etl" {
+  name   = aws_iam_role.paintings_etl.name
+  role   = aws_iam_role.paintings_etl.id
+  policy = data.aws_iam_policy_document.paintings_etl.json
 }
 
-resource "aws_iam_role_policy_attachment" "csv_paintings_crawler_glue" {
-  role       = aws_iam_role.csv_paintings_crawler.id
+resource "aws_iam_role_policy_attachment" "paintings_etl_glue" {
+  role       = aws_iam_role.paintings_etl.id
   policy_arn = local.glue_service_role_arn
 }
 
-resource "aws_glue_crawler" "csv_paintings" {
+resource "aws_glue_crawler" "csv_paintings_source" {
   database_name = aws_glue_catalog_database.paintings.name
-  name          = "csv-paintings"
-  role          = aws_iam_role.csv_paintings_crawler.arn
+  name          = "csv-paintings-source"
+  role          = aws_iam_role.paintings_etl.arn
   tags          = local.tags
 
   s3_target {
@@ -54,47 +67,20 @@ resource "aws_s3_bucket" "paintings_processed" {
   force_destroy = true
 }
 
-data "aws_iam_policy_document" "process_csv_paintings" {
-  statement {
-    actions   = ["s3:HeadObject", "s3:ListObjects", "s3:GetObject"]
-    resources = [aws_s3_bucket.glue_jobs_source.arn, "${aws_s3_bucket.glue_jobs_source.arn}/*"]
-  }
-  statement {
-    actions   = ["s3:HeadObject", "s3:GetObject"]
-    resources = ["${aws_s3_bucket.paintings_source.arn}/*"]
-  }
-  statement {
-    actions = ["s3:ListBucket", "s3:PutObject", "s3:DeleteObject"]
-    resources = [
-      aws_s3_bucket.paintings_processed.arn,
-      "${aws_s3_bucket.paintings_processed.arn}/*",
-      aws_s3_bucket.glue_temp_files.arn,
-      "${aws_s3_bucket.glue_temp_files.arn}/*",
-    ]
-  }
-}
+resource "aws_glue_crawler" "csv_paintings_processed" {
+  database_name = aws_glue_catalog_database.paintings.name
+  name          = "csv-paintings-processed"
+  role          = aws_iam_role.paintings_etl.arn
+  tags          = local.tags
 
-resource "aws_iam_role" "process_csv_paintings" {
-  name                  = "${var.namespace}-process-csv-paintings"
-  assume_role_policy    = data.aws_iam_policy_document.glue_assume_role.json
-  force_detach_policies = true
-  tags                  = merge(local.tags, { Name = "${var.namespace}-process-csv-paintings" })
-}
-
-resource "aws_iam_role_policy" "process_csv_paintings" {
-  name   = aws_iam_role.process_csv_paintings.name
-  role   = aws_iam_role.process_csv_paintings.id
-  policy = data.aws_iam_policy_document.process_csv_paintings.json
-}
-
-resource "aws_iam_role_policy_attachment" "process_csv_paintings_glue" {
-  role       = aws_iam_role.process_csv_paintings.id
-  policy_arn = local.glue_service_role_arn
+  s3_target {
+    path = "s3://${aws_s3_bucket.paintings_processed.bucket}"
+  }
 }
 
 resource "aws_glue_job" "process_csv_paintings" {
   name              = "process-s3-paintings"
-  role_arn          = aws_iam_role.process_csv_paintings.arn
+  role_arn          = aws_iam_role.paintings_etl.arn
   glue_version      = "2.0"
   number_of_workers = 10
   worker_type       = "G.1X"
@@ -108,8 +94,56 @@ resource "aws_glue_job" "process_csv_paintings" {
     "--extra-py-files" = "s3://${aws_s3_bucket.glue_jobs_source.bucket}/cory.etl-0.0.1-py3-none-any.whl"
     "--TempDir"        = "s3://${aws_s3_bucket.glue_temp_files.bucket}/process-s3-paintings/"
     # Underscores used for custom params per this doc: https://docs.aws.amazon.com/glue/latest/dg/aws-glue-api-crawler-pyspark-extensions-get-resolved-options.html
-    "--input_database"   = aws_glue_crawler.csv_paintings.database_name
-    "--input_table"      = aws_s3_bucket.paintings_source.bucket
+    "--input_database"   = aws_glue_crawler.csv_paintings_source.database_name
+    "--input_table"      = replace(aws_s3_bucket.paintings_source.bucket, "-", "_") # Glue crawler naming convention uses underscores on created tables
     "--processed_bucket" = aws_s3_bucket.paintings_processed.bucket
+  }
+}
+
+resource "aws_glue_workflow" "process_paintings" {
+  name = "${var.namespace}-process-paintings"
+}
+
+resource "aws_glue_trigger" "run_paintings_source_crawler" {
+  name          = "${var.namespace}-run-source-crawler"
+  type          = "ON_DEMAND"
+  workflow_name = aws_glue_workflow.process_paintings.name
+
+  actions {
+    crawler_name = aws_glue_crawler.csv_paintings_source.name
+  }
+}
+
+resource "aws_glue_trigger" "run_process_csv_paintings" {
+  name          = "${var.namespace}-run-process-csv-job"
+  type          = "CONDITIONAL"
+  workflow_name = aws_glue_workflow.process_paintings.name
+
+  actions {
+    job_name = aws_glue_job.process_csv_paintings.name
+  }
+
+  predicate {
+    conditions {
+      crawler_name = aws_glue_crawler.csv_paintings_source.name
+      crawl_state  = "SUCCEEDED"
+    }
+  }
+}
+
+resource "aws_glue_trigger" "run_paintings_processed_crawler" {
+  name          = "${var.namespace}-run-processed-crawler"
+  type          = "CONDITIONAL"
+  workflow_name = aws_glue_workflow.process_paintings.name
+
+  actions {
+    crawler_name = aws_glue_crawler.csv_paintings_processed.name
+  }
+
+  predicate {
+    conditions {
+      job_name = aws_glue_job.process_csv_paintings.name
+      state    = "SUCCEEDED"
+    }
   }
 }
